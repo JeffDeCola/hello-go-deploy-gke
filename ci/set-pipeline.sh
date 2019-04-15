2975f70ea099a341b8eb64f29992e8169f921270
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeffdecola@gmail.com>
2019-04-15 14:02:12 -0700

    updating concourse deploy - creating gcloud-kubectl docker image

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # hello-go-deploy-gke set-pipeline.sh
 
-fly -t ci set-pipeline -p hello-go-deploy-gke -c pipeline.yml --load-vars-from ../../../../../.credentials.yml
+fly -t ci set-pipeline -p hello-go-deploy-gke -c pipeline.yml \
+    --load-vars-from ../../../../../.credentials.yml \
+    --var "gcp_jeffs_app_service_account_file=$(cat $GCP_JEFFS_APP_SERVICE_ACCOUNT_PATH | base64)" \
+    --var "gcp_jeffs_app_service_account_email_address=$GCP_JEFFS_APP_SERVICE_ACCOUNT_EMAIL_ADDRESS" \
+    --var "gcp_jeffs_project_id=$GCP_JEFFS_PROJECT_ID"
+    
